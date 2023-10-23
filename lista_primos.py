9abaf65cb524fc4b740888db7869d709b90bc8ba
Moni-ib <monica.ibarra@iteso.mx>
2023-10-23 16:20:49 -0600

    p1

--- lista_primos.py
+++ lista_primos.py
@@ -3,13 +3,21 @@ Inserta el encabezado aquí y escribe tu código abajo
 """
 
 # Declaraciones
-CONSTANTE = valor
-
+divisores = []
 # Entradas
-entrada = input()
+entrada = int(input("Introduzca un número: "))
 
 # Proceso
+divisor = 2
+for numero in range (1, entrada + 1):
+    while divisor < entrada:
+        if entrada % divisor == 0 and divisor != entrada:
+            break
+        divisor += 1
+    else:
+        divisores.append(divisor)
+        print(f"Los números primos menores o igaules a {entrada} son: {divisores}")
 
 
-# Salidas
-print(salida)
+        
+
