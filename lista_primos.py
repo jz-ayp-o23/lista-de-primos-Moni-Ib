19cb47b34d74c25d486337b5704a136a1a5b71d9
Moni-ib <monica.ibarra@iteso.mx>
2023-10-23 16:38:43 -0600

    true

--- lista_primos.py
+++ lista_primos.py
@@ -10,13 +10,15 @@ entrada = int(input("Introduzca un número: "))
 
 # Proceso
 for numero in range (2, entrada + 1):
-    resultado = 0
+    resultado = True
     for divisor in range(2, numero):
         if numero % divisor == 0:
+            resultado = False
             break
-        else:
+        if resultado == True:
             numeros.append(numero)
-            print(f"Los números primos menores o iguales a {entrada} son: {numeros}")
+
+print(f"Los números primos menores o iguales a {entrada} son: {numeros}")
 
 
         
